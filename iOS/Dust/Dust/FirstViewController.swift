3f74e545fc5189c169fc7b62bba88e07bf32b537
kjoonk <kjoonk@gmail.com>
2020-03-31 21:03:37 +0900

    ViewController에서 DustState 데이터와 View를 연결하여 표시 확인

--- iOS/Dust/Dust/FirstViewController.swift
+++ iOS/Dust/Dust/FirstViewController.swift
@@ -3,12 +3,28 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    @IBOutlet weak var stateSymbol: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var dustValue: UILabel!
+    @IBOutlet weak var dateTimeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let dustState = DustState(dateTime: Date(), value: 3.14, pm10Grade1h: 1)
+
+        stateSymbol.text = dustState.grade.symbol
+        stateLabel.text = dustState.grade.label
+        dustValue.text = "\(dustState.value) µg/m³"
+
+        // dateformat
+        let dateFormatter = DateFormatter()
+        dateFormatter.doesRelativeDateFormatting = true
+        dateFormatter.locale = Locale(identifier: "ko")
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .short
+
+        dateTimeLabel.text = dateFormatter.string(from: dustState.dateTime)
     }
-
-
 }
 
