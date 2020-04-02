5b346e170845c6a76b6ffbbd312b3038bfaade31
kjoonk <kjoonk@gmail.com>
2020-04-02 22:01:11 +0900

    Gradient 기능 구현 시험
    - 예보 탭에 임시 뷰를 만들어 시험

---
+++ iOS/Dust/Dust/ViewController/ForecastViewController.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+class ForecastViewController: UIViewController {
+    @IBOutlet weak var gradientView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let blueColor = UIColor(hex: "#3D85DD")?.cgColor {
+            self.addGradientLayer(in: self.gradientView, startColor: blueColor)
+        }
+    }
+
+    func addGradientLayer(in view: UIView, startColor: CGColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        gradientLayer.colors = [ startColor, UIColor.white.cgColor ]
+        gradientLayer.shouldRasterize = true
+        view.layer.addSublayer(gradientLayer)
+    }
+
+}
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...]).lowercased()
+
+            if hexColor.count == 6 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: 1)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+}
--- iOS/Dust/Dust/ViewController/StateViewController.swift
+++ iOS/Dust/Dust/ViewController/StateViewController.swift
@@ -1,46 +1,24 @@
 import UIKit
 
-class DustStateViewController: UIViewController {
+class StateViewController: UIViewController {
 
     // MARK: Properties
 
-    @IBOutlet weak var barChartTableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     let cellIdentifier: String = "BarCell"
     var dustStates = [DustState]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        barChartTableView.dataSource = self
+        tableView.dataSource = self
 
         // MARK: HTTPRequest JSON
-        let request = HTTPRequest()
-        guard let url = URL(string: "https://dust10.herokuapp.com/api/dust-status") else { return }
-
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(DateFormatter.yyyyMMdd)
-        request.getJSON(url: url, decoder: decoder) { (result: Result<DustStates, HTTPRequest.APIError>) in
-            var dustState: DustState
-            switch result {
-            case .success(let dustStates):
-                self.dustStates = dustStates.list
-                DispatchQueue.main.async {
-                    self.barChartTableView.reloadData()
-                }
-                dustState = self.dustStates.first!
-            case .failure(let error):
-                print(error.localizedDescription)
-                #warning("테스트 데이터")
-                dustState = DustState(measuredTime: Date(), value: 314, originalGrade: 2)
-            }
-            self.updateMainView(dataSource: dustState)
-        }
+        fetchStates()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-
-
     }
 
     // MARK: Private Methods
@@ -52,15 +30,40 @@ class DustStateViewController: UIViewController {
             }
         }
     }
+
+    private func fetchStates() {
+        let request = HTTPRequest()
+        guard let url = URL(string: "https://dust10.herokuapp.com/api/dust-status") else { return }
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.yyyyMMdd)
+
+        request.getJSON(url: url, decoder: decoder) { (result: Result<DustStates, HTTPRequest.APIError>) in
+            var dustState: DustState
+            switch result {
+            case .success(let dustStates):
+                self.dustStates = dustStates.list
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                dustState = self.dustStates.first!
+            case .failure(let error):
+                print(error.localizedDescription)
+                #warning("테스트 데이터")
+                dustState = DustState(measuredTime: Date(), value: 314, originalGrade: 2)
+            }
+            self.updateMainView(dataSource: dustState)
+        }
+    }
 }
 
-extension DustStateViewController: UITableViewDataSource {
+extension StateViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.dustStates.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = barChartTableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! BarChartCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! BarChartCell
         let dustState = self.dustStates[indexPath.row]
 
         cell.dustValue.text = String(dustState.value ?? 0)
