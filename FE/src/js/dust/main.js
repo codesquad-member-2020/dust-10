0e189599a196dbe45570ee6baa4ea3670cfbbeb2
최장호 <hmchoi651@naver.com>
2020-04-03 14:21:54 +0900

    [삭제 , 수정] getlocation 함수 삭제 , fetchDustList 함수 실행문 추가
    getlocation : htttp server에서 사용할 수 없는 로직으로 인한 삭제
    fetchDustList : 고정 위도 경도값으로 즉시 실행하도록 변경

--- FE/src/js/dust/main.js
+++ FE/src/js/dust/main.js
@@ -1,34 +1,20 @@
 import { renderStationList } from "../dust/dustGrape.js";
 import { changeGradeInfo, renderStation } from "../dust/dustGrade.js";
 import { initTouchEvent } from "../dust/dustTouch.js";
+
 import DOM from "../../option/dustPageDom.js";
+import COORDIMATE from "../../constants/coordinate.js";
 
 const url = "http://127.0.0.1:5501/src/data/mockdata.json";
-
-function getLocation() {
-  if (navigator.geolocation) {
-    // GPS를 지원하면
-    navigator.geolocation.getCurrentPosition(
-      function(position) {
-        fetchDustList(position.coords.latitude, position.coords.longitude);
-      },
-      function(error) {
-        console.error(error);
-      }
-    );
-  } else {
-    alert("GPS를 지원하지 않습니다");
-  }
-}
-
-getLocation();
 
 async function fetchDustList(latitude, longitude) {
   const localUrl = `http://13.125.3.28:8080/api/location?longitude=${longitude}&latitude=${latitude}`;
   const localResponse = await fetch(localUrl);
   const localJSON = await localResponse.json();
   const localStationName = localJSON.stationName;
+
   renderStation(localStationName);
+
   const dataListUrl = `http://13.125.3.28:8080/api/dust-status?stationName=${localStationName}`;
 
   try {
@@ -43,3 +29,4 @@ async function fetchDustList(latitude, longitude) {
     alert(`Faile to fetch`);
   }
 }
+fetchDustList(COORDIMATE.FIX_LATITUDE, COORDIMATE.FIX_LONGITUDE);
