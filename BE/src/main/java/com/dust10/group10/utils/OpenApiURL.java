6467217e6563efa289bc6d42c1ac15c8410aad50
Sosah92 <thstkd92@gmail.com>
2020-04-11 13:55:16 +0900

    Update OpenApiURL.java

--- BE/src/main/java/com/dust10/group10/utils/OpenApiURL.java
+++ BE/src/main/java/com/dust10/group10/utils/OpenApiURL.java
@@ -7,5 +7,4 @@ public class OpenApiURL {
     protected static final String FORECAST_OPEN_API_URL = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth";
 
     protected static final String KAKAO_KEY = "KakaoAK c2de27fae60a8e007a44e7d8a411efec";
-    protected static final String SERVICE_KEY = "pd7oMfpEAuYkyy7Mm3b9cZfe49GY1p%2B4EKHYoipIgk9RrZfCitVfVH3LGPo%2Fb93v9%2FHPnQlgOX7stocZtomyeQ%3D%3D";
-}
+    protected static final String SERVICE_KEY =
